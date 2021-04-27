eead437f90829ebb8c147b7eea2672c254e52f3d
Maksym Yurchenko <mvy@norse.co>
2021-04-27 15:26:09 +0300

    fix to cookie processor

--- app/src/main/resources/site/processors/ckeckCookie.js
+++ app/src/main/resources/site/processors/ckeckCookie.js
@@ -2,17 +2,19 @@ var libLocation = "/site/lib/";
 var norseUtils = require(libLocation + "norseUtils");
 
 exports.responseProcessor = function (req, res) {
-  const sessionCookieAmount = (req.headers.Cookie.match(/JSESSIONID/g) || [])
-    .length;
-  if (req.cookies.JSESSIONID && sessionCookieAmount === 1)
-    res.cookies = {
-      JSESSIONID: {
-        value: req.cookies.JSESSIONID,
-        path: "/",
-        secure: false,
-        httpOnly: true,
-        domain: "." + app.config.baseHost
-      }
-    };
+  if (req && req.headers && req.headers.Cookie) {
+    const sessionCookieAmount = (req.headers.Cookie.match(/JSESSIONID/g) || [])
+      .length;
+    if (req.cookies.JSESSIONID && sessionCookieAmount === 1)
+      res.cookies = {
+        JSESSIONID: {
+          value: req.cookies.JSESSIONID,
+          path: "/",
+          secure: false,
+          httpOnly: true,
+          domain: "." + app.config.baseHost
+        }
+      };
+  }
   return res;
 };
