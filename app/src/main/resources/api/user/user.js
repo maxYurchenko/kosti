264c11853b14c05c305765d9077cd71e16b0253f
Maksym Yurchenko <mvy@norse.digital>
2022-02-12 15:39:10 +0200

    added uer ticket assignments

--- app/src/main/resources/api/user/user.js
+++ app/src/main/resources/api/user/user.js
@@ -1,13 +1,25 @@
-var contentLib = require("/lib/xp/content");
-var thymeleaf = require("/lib/thymeleaf");
-var portal = require("/lib/xp/portal");
-
-var libLocation = "/site/lib/";
-var userLib = require("/lib/userLib");
+const userLib = require("/lib/userLib");
+const playerLib = require("/lib/festival/playerLib");
+const norseUtils = require("../../site/lib/norseUtils");
 
 exports.post = function (req) {
+  if (req.params.kosticonnect2022) {
+    if (!parseInt(req.params.kosticonnect2022)) {
+      return {
+        body: { error: true, message: "Не верный билет." },
+        contentType: "application/json"
+      };
+    }
+    const assingnedTicket = playerLib.assignTicket(req.params.kosticonnect2022);
+    if (assingnedTicket.error) {
+      return {
+        body: assingnedTicket,
+        contentType: "application/json"
+      };
+    }
+  }
   return {
-    body: userLib.editUser(req.params),
+    body: { error: false, data: userLib.editUser(req.params) },
     contentType: "application/json"
   };
 };
